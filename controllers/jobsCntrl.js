6496817daadd57b5ff183184da6d8bf8d8f5bb42
Coding_Learning <soumyadip.mondal@gmail.com>
2024-09-21 08:44:21 +0530

    NODE - CRUD operation built

--- controllers/jobsCntrl.js
+++ controllers/jobsCntrl.js
@@ -1,19 +1,23 @@
 const Job = require("../db-models/jobSchema");
 
 /* Get jobs controller */
-
-exports.getJobs = (req, res, next) => {
+/* Get all jobs */
+exports.getJobs = async (req, res, next) => {
+  const allJobs = await Job.find(); //Find
   res.status(200).json({
     success: true,
     methodName: req.reqMthd,
     url: req.URL,
-    message: "Routes to get all jobs",
+    message: "All jobs Fetched",
+    data: allJobs,
   });
 };
 
+/* Creation of jobs */
+
 exports.newJob = async (req, res, next) => {
   console.log("params -> ", req.body);
-  const job = await Job.create(req.body);
+  const job = await Job.create(req.body); //creation
 
   res.status(200).json({
     success: true,
@@ -22,4 +26,46 @@ exports.newJob = async (req, res, next) => {
     message: "New job is added",
     data: job,
   });
+};
+
+/* Updating a job */
+
+exports.updateJob = async (req, res, next) => {
+  const updateJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  if (!updateJob) {
+    res.status(404).json({
+      sucess: false,
+      message: "No Relevant job found",
+    });
+  } else {
+    res.status(200).json({
+      sucess: true,
+      message: "Job details Successfully updated",
+      data: updateJob,
+    });
+  }
+};
+
+/* Deleting a job */
+
+exports.deleteJob = async (req, res, next) => {
+  const deletedJob = await Job.findByIdAndDelete(req.params.id);
+
+  if (!deletedJob) {
+    res.status(404).json({
+      sucess: false,
+      message: "No Relevant job found for deletion",
+    });
+  } else {
+    res.status(200).json({
+      sucess: true,
+      message: "Job details Successfully deleted",
+      data: deletedJob,
+    });
+  }
 };
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,16 +1,30 @@
 const express = require("express");
-const { getJobs, newJob } = require("../controllers/jobsCntrl");
+const {
+  getJobs,
+  newJob,
+  updateJob,
+  deleteJob,
+} = require("../controllers/jobsCntrl");
 
 const router = express.Router();
 
 //router.get("/jobs", (req, res) => {});
 
-/* Using controller technique */
+/* Using controller technique CRUD operations */
 /* getting jobs */
+
 router.route("/jobs").get(getJobs);
 
 /* setting jobs */
 
 router.route("/new/job").post(newJob);
+
+/* Update a job details */
+
+router.route("/job/update/:id").put(updateJob);
+
+/* Delete a job */
+
+router.route("/job/delete/:id").delete(deleteJob);
 
 module.exports = router;
