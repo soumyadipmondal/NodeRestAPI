86b35327c3e01f485b1b67cb6fd601a105589179
Coding_Learning <soumyadip.mondal@gmail.com>
2024-09-21 08:54:55 +0530

    NODE - Code refactor

--- controllers/jobsCntrl.js
+++ controllers/jobsCntrl.js
@@ -1,31 +1,34 @@
 const Job = require("../db-models/jobSchema");
 
 /* Get jobs controller */
+
+const notFoud = (res) => {
+  res.status(404).json({
+    sucess: false,
+    message: "Error - Not Found",
+  });
+};
+
+const successSubmission = (res, job) => {
+  res.status(200).json({
+    success: true,
+    message: "Operation Successful",
+    data: job,
+  });
+};
+
 /* Get all jobs */
 exports.getJobs = async (req, res, next) => {
   const allJobs = await Job.find(); //Find
-  res.status(200).json({
-    success: true,
-    methodName: req.reqMthd,
-    url: req.URL,
-    message: "All jobs Fetched",
-    data: allJobs,
-  });
+
+  successSubmission(res, allJobs);
 };
 
 /* Creation of jobs */
 
 exports.newJob = async (req, res, next) => {
-  console.log("params -> ", req.body);
-  const job = await Job.create(req.body); //creation
-
-  res.status(200).json({
-    success: true,
-    methodName: req.reqMthd,
-    url: req.URL,
-    message: "New job is added",
-    data: job,
-  });
+  const newJob = await Job.create(req.body); //creation
+  successSubmission(res, newJob);
 };
 
 /* Updating a job */
@@ -38,16 +41,9 @@ exports.updateJob = async (req, res, next) => {
   });
 
   if (!updateJob) {
-    res.status(404).json({
-      sucess: false,
-      message: "No Relevant job found",
-    });
+    notFoud(res);
   } else {
-    res.status(200).json({
-      sucess: true,
-      message: "Job details Successfully updated",
-      data: updateJob,
-    });
+    successSubmission(res, updateJob);
   }
 };
 
@@ -55,17 +51,9 @@ exports.updateJob = async (req, res, next) => {
 
 exports.deleteJob = async (req, res, next) => {
   const deletedJob = await Job.findByIdAndDelete(req.params.id);
-
   if (!deletedJob) {
-    res.status(404).json({
-      sucess: false,
-      message: "No Relevant job found for deletion",
-    });
+    notFoud(res);
   } else {
-    res.status(200).json({
-      sucess: true,
-      message: "Job details Successfully deleted",
-      data: deletedJob,
-    });
+    successSubmission(res, deletedJob);
   }
 };
